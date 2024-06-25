6a46cebfe27f823a844b8ac110fe67cedad6ce16
Luiz Felipe <luiz.terrasilva22@gmail.com>
2024-06-24 22:11:41 -0300

    Inicio

---
+++ app/src/main/java/com/example/newsappapi/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.newsappapi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newsappapi.Models.NewsApiResponse;
+import com.example.newsappapi.Models.NewsHeadlines;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    CustomAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, "general", null);
+    }
+
+    private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
+        @Override
+        public void onFetchData(List<NewsHeadlines> list, String message) {
+            showNews(list);
+        }
+
+        @Override
+        public void onError(String message) {
+
+        }
+    };
+
+    private void showNews(List<NewsHeadlines> list) {
+        recyclerView = findViewById(R.id.recycler_main);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        adapter = new CustomAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/newsappapi/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.newsappapi;
+
+import com.example.newsappapi.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse>{
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
