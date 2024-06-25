8e0829dab26657f73d05ea60f7b86a9b64ce3dd8
Luiz Felipe <luiz.terrasilva22@gmail.com>
2024-06-25 11:10:21 -0300

    Buttons

--- app/src/main/java/com/example/newsappapi/MainActivity.java
+++ app/src/main/java/com/example/newsappapi/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.newsappapi;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,14 +21,55 @@ import com.example.newsappapi.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener {
     RecyclerView recyclerView;
     CustomAdapter adapter;
+    ProgressDialog dialog;
+    Button b1, b2, b3, b4, b5, b6, b7;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Buscando Artigos de " + query);
+                dialog.show();
+
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getNewsHeadlines(listener, "general", query);
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Buscando artigos de notícias...");
+        dialog.show();
+
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
 
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, "general", null);
@@ -31,12 +78,17 @@ public class MainActivity extends AppCompatActivity {
     private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
+            if (list.isEmpty()){
+                Toast.makeText(MainActivity.this, "Nenhum Artigo encontrado!", Toast.LENGTH_SHORT).show();
+            } else {
+                showNews(list);
+                dialog.dismiss();
+            }
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "Erro!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -44,7 +96,24 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_main);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-        adapter = new CustomAdapter(this, list);
+        adapter = new CustomAdapter(this, list, this);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void OnNewsClicked(NewsHeadlines headlines) {
+        startActivity(new Intent(MainActivity.this, DetailsActivity.class)
+                .putExtra("data", headlines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+        dialog.setTitle("Buscando os artigos de " + category);
+        dialog.show();
+
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
---
+++ app/src/main/java/com/example/newsappapi/SelectListener.java
@@ -0,0 +1,8 @@
+package com.example.newsappapi;
+
+import com.example.newsappapi.Models.NewsHeadlines;
+
+public interface SelectListener {
+
+    void OnNewsClicked(NewsHeadlines headlines);
+}
